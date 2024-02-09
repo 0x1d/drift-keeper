1ccc5f8614b52886be89a63315655d9bd61c6ae4
Patrick Balsiger <frupii@gmail.com>
2024-02-09 21:22:19 +0100

    Refactor ctl commands

--- ctl.sh
+++ ctl.sh
@@ -8,13 +8,15 @@ source .env
 ##
 ## Usage: ./ctl.sh COMMAND SUBCOMMAND
 ##
-## ~> keeper
-##    build             Build bot image
-##    push              Push bot image to Docker registry
+## ~> build
+##    keeper            Build bot image
+##    tracker           Buils wallet-tracker image
 ##
-## ~> tracker
-##    build             Build wallet tracker image
-##    push              Push tracker image to Docker registry
+## ~> push
+##    keeper            Push bot image to Docker registry
+##    tracker           Push tracker image to Docker registry
+##
+## ~> run               Run the stack locally
 ##
 ## ~> infra
 ##    plan              Plan infrastructure change
@@ -41,29 +43,33 @@ function info {
     sed -n 's/^##//p' ctl.sh
 }
 
-function keeper {
-    function build {
+function build {
+    function keeper {
         mkdir -p .build
         git clone https://github.com/drift-labs/keeper-bots-v2 -b mainnet-beta .build/keeper-bots-v2
         docker build -f Dockerfile -t ${DOCKER_IMAGE} .build/keeper-bots-v2
         rm -rf .build
     }
-    function push {
-        docker push ${DOCKER_IMAGE}
-    }
-    ${@:-}
-}
-
-function tracker {
-    function build {
+    function tracker {
         pushd wallet-tracker
             docker build -t ${DOCKER_IMAGE_WALLET_TRACKER} .
         popd
     }
-    function push {
+    ${@:-}
+}
+
+function push {
+    function keeper {
+        docker push ${DOCKER_IMAGE}
+    }
+    function tracker {
         docker push ${DOCKER_IMAGE_WALLET_TRACKER}
     }
     ${@:-}
+}
+
+function run {
+    docker compose up
 }
 
 function infra {
